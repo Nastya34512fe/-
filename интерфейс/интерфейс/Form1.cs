575c1f50fe4f88d283291f0ee0091f80a68613ff
makuma135 <forworksajty@gmail.com>
2026-03-27 17:42:22 +0500

    Update Form1.cs
    изменила регистр "T"

--- интерфейс/интерфейс/Form1.cs
+++ интерфейс/интерфейс/Form1.cs
@@ -15,7 +15,7 @@ namespace интерфейс
 {
     public partial class Form1 : Form
     {
-        private string Filetext;
+        private string FileText;
         public Form1()
         {
             InitializeComponent();
